f5c97fd85712360fb6503c57023c057f74ce091b
Cjsah <cjsah@cjsah.net>
2024-09-04 20:00:46 +0800

    update

--- src/views/mainwindow.cpp
+++ src/views/mainwindow.cpp
@@ -1,20 +1,20 @@
 #include "mainwindow.h"
 #include "./design/ui_mainwindow.h"
 #include<QDebug>
+#include <QLoggingCategory>
 
 template<typename ...T>
 void initTable(QTableWidget *table, T... args);
-void initHeader(QHeaderView *header);
 void setItem(QTableWidget *table, int x, int y, const QString& text);
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    QLoggingCategory::defaultCategory()->setEnabled(QtDebugMsg, true);
     ui->setupUi(this);
-    this->setWindowFlags(Qt::WindowMinimizeButtonHint);
     this->setFixedSize(this->width(), this->height());
-    initTable(ui->genshin_boss_material, 132, 108, 20, 20, 20);
+    initTable(ui->genshin_boss_material, 132, 108, 30, 30, 30);
 }
 
 MainWindow::~MainWindow()
@@ -25,8 +25,7 @@ MainWindow::~MainWindow()
 template<typename ...T>
 void initTable(QTableWidget *table, T... args)
 {
-    table->verticalHeader()->setVisible(false);
-    initHeader(table->horizontalHeader());
+    table->horizontalHeader()->setSectionResizeMode(QHeaderView::Fixed);
     table->setRowCount(1);
     const std::initializer_list<int> list{args...};
     int index = 0;
@@ -34,17 +33,14 @@ void initTable(QTableWidget *table, T... args)
     {
         table->setColumnWidth(index++, width);
     }
+
+
+
     setItem(table, 0, 0, "机械发条备件-格菲利亚");
     setItem(table, 0, 1, "冰封组曲-格菲利亚");
     setItem(table, 0, 2, "999");
     setItem(table, 0, 3, "999");
     setItem(table, 0, 4, "999");
-}
-
-void initHeader(QHeaderView *header)
-{
-    header->setHighlightSections(false);
-    header->setSectionResizeMode(QHeaderView::Fixed);
 }
 
 void setItem(QTableWidget *table, const int x, const int y, const QString& text)
